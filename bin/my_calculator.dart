1ec30ede2c3a609a90807d9b02654dcfda944b73
https://github.com/Cholponai01 <cholponai.amangeldikyzy.bj@gmail.com>
2023-12-06 20:14:26 +0600

    done

--- bin/my_calculator.dart
+++
@@ -1,5 +0,0 @@
-// import 'package:my_calculator/my_calculator.dart' as my_calculator;
-
-// void main(List<String> arguments) {
-//   print('Hello world: ${my_calculator.calculate()}!');
-// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,57 @@
-import 'my_calculator.dart';
+import 'dart:io';
+
+class Math {
+  double add(double a, double b) => a + b;
+  double subtract(double a, double b) => a - b;
+  double multiply(double a, double b) => a * b;
+  double divide(double a, double b) {
+    if (b != 0) {
+      return a / b;
+    } else {
+      print("Ошибки деление на ноль!");
+      return double.nan;
+    }
+  }
+}
+
+class Screen {
+  double getNumberInput(String prompt) {
+    stdout.write(prompt);
+    String userInput = stdin.readLineSync()!;
+    return double.parse(userInput);
+  }
+
+  String getOperationInput() {
+    stdout.write("Выберите операцию (+, -, *, /):");
+    String operation = stdin.readLineSync()!.replaceAll(",", ".");
+    return operation;
+  }
+
+  void displayResult(double result) {
+    print("Результат: $result");
+  }
+}
 
 void main() {
-  int num1 = 10;
-  int num2 = 5;
+  Math math = Math();
+  Screen screen = Screen();
 
-  int resultAdd = add(num1, num2);
-  int resultSubtract = subtract(num1, num2);
-  int resultMultiply = multiply(num1, num2);
-  int resultDivide = divide(num1, num2);
+  double num1 = screen.getNumberInput("Введите первое число: ");
+  double num2 = screen.getNumberInput("Введите второе число: ");
+  String operation = screen.getOperationInput();
+  double result;
 
-  print("Addition: $resultAdd");
-  print("Subtraction: $resultSubtract");
-  print("Multiplication: $resultMultiply");
-  print("Division: $resultDivide");
+  Map<String, double Function(double, double)> operations = {
+    "+": math.add,
+    "-": math.subtract,
+    "*": math.multiply,
+    "/": math.divide,
+  };
+  if (operations.containsKey(operation)) {
+    result = operations[operation]!(num1, num2);
+  } else {
+    print("Ошибка: неверная операция!");
+    return;
+  }
+  screen.displayResult(result);
 }
